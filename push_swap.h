a5aa9f0c1b18ddf9cceffa22ae30d3670027c3d3
tomofuji555 <tomokifujiwara321@outlook.jp>
2023-06-28 17:37:46 +0900

    4

---
+++ push_swap.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/26 20:24:00 by marvin            #+#    #+#             */
+/*   Updated: 2023/06/26 20:24:00 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef PUSH_SWAP
+# define PUSH_SWAP
+
+typedef int data_t;
+
+# include   <stdio.h>
+# include	<limits.h>
+
+typedef struct s_list{
+	void			*content;
+    int				index;
+    int				size;
+    struct  s_list	*next;
+} t_list;
+
+char	**check_param(int argc, char **argv, t_stack **a);
+
+
+# endif
